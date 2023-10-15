8b4b95b252f1216fce4b4a887ffe157790cb1969
Poojanpatel2003 <144653133+Poojanpatel2003@users.noreply.github.com>
2023-10-15 16:01:30 +0530

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-let typed = new Typed(".text", {
-    strings: ["Student from Parul University", "frontend developer", "Web developer"],
-    typeSpeed: 120,
-    backSpeed: 150,
-    backDelay: 1600,
-    loop: true
-});
